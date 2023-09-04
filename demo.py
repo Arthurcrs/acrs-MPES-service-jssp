c9619aa18c8832e4d883df4d67e658ecc8219775
Arthur Carlos da Rocha Silva <arthcrs@gmail.com>
2023-09-04 17:25:05 -0300

    Added constraint variable

--- demo.py
+++ demo.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 from dimod.reference.samplers import ExactSolver
 from dwave.system.composites import EmbeddingComposite
 from dwave.system.samplers import DWaveSampler
-from job_shop_scheduler import get_jss_bqm, is_auxiliary_variable
+from job_shop_scheduler import get_jss_bqm
 from utils import *
 
 # Problem Definition
@@ -11,13 +11,13 @@ jobs = {"job_1": [("machine_1", 2)],
         "job_2": [("machine_2", 1),("machine_3", 1)],
         "job_3": [("machine_3", 2)]}
 
-machine_downtimes = {"machine_3" : [0,1,5],
-                     "machine_1" : [1,2,5]}
+# machine_downtimes = {"machine_3" : [0,1,5],
+#                      "machine_1" : [1,2,5]}
 
 # Construct a BQM for the jobs
 
 max_time = 6
-bqm = get_jss_bqm(jobs,machine_downtimes, max_time)
+bqm = get_jss_bqm(jobs, max_time)
 
 # # Submit BQM
 
@@ -39,31 +39,31 @@ file = open("solution.txt", "w")
 file.write(str(solution))
 file.close()
 
-selected_nodes = [k for k, v in solution.sample.items() if v == 1]
+# selected_nodes = [k for k, v in solution.sample.items() if v == 1]
 
-# Parse node information
-task_times = {k: [-1]*len(v) for k, v in jobs.items()}
-tasks_test = []
-print(task_times)
-for node in selected_nodes:
-    if is_auxiliary_variable(node):
-        continue
-    job_name, task_time = node.rsplit("_", 1)
-    task_index, start_time = map(int, task_time.split(","))
+# # Parse node information
+# task_times = {k: [-1]*len(v) for k, v in jobs.items()}
+# tasks_test = []
+# print(task_times)
+# for node in selected_nodes:
+#     if is_auxiliary_variable(node):
+#         continue
+#     job_name, task_time = node.rsplit("_", 1)
+#     task_index, start_time = map(int, task_time.split(","))
 
-    task_times[job_name][task_index] = start_time
+#     task_times[job_name][task_index] = start_time
 
-# Print problem and restructured solution
-print("Jobs and their machine-specific tasks:")
+# # Print problem and restructured solution
+# print("Jobs and their machine-specific tasks:")
 
-for job, task_list in jobs.items():
-    print("{0:9}: {1}".format(job, task_list))
+# for job, task_list in jobs.items():
+#     print("{0:9}: {1}".format(job, task_list))
 
-print("\nJobs and the start times of each task:")
-for job, times in task_times.items():
-    print("{0:9}: {1}".format(job, times))
-    for time in times:
-        tasks_test.append([job,time])
+# print("\nJobs and the start times of each task:")
+# for job, times in task_times.items():
+#     print("{0:9}: {1}".format(job, times))
+#     for time in times:
+#         tasks_test.append([job,time])
 
-print("\nMachines and their downtimes")
-print(machine_downtimes)
+# print("\nMachines and their downtimes")
+# print(machine_downtimes)
